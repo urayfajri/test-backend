42625c7ec91640c42a57e6069e1ee73dbc3c1103
WDTI0\uray.widiansyah <uray.widiansyah@primecash.co.id>
2025-02-26 17:48:12 +0700

    feat: connect supabase

--- functions/api.js
+++ functions/api.js
@@ -1,10 +1,26 @@
 const express = require("express");
 
 const serverless = require("serverless-http");
- 
+
+const { createClient } = require("@supabase/supabase-js");
+
 const app = express();
 
 app.use(express.json());
+
+// Inisialisasi Supabase Client
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+// Endpoint GET: Ambil data dari tabel "users"
+app.get("/api/v1/items", async (req, res) => {
+  const { data, error } = await supabase.from("master_item").select("*");
+  if (error) return res.status(400).json({ error: error.message });
+
+  res.json(data);
+});
  
 app.get("/api/v1/", (req, res) => {
 
@@ -19,7 +35,6 @@ app.post("/data", (req, res) => {
 });
  
 // Bungkus Express dengan serverless
-
 module.exports.handler = serverless(app);
 
  
